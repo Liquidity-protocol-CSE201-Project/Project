6f40b34bc4aebcfcfeeb78fdb2b00671e1166351
alemassaad <115092467+alemassaad@users.noreply.github.com>
2022-12-05 15:22:22 +0100

    Update Graphs.hpp

--- src/FrontEnd/Graphs.hpp
+++ src/FrontEnd/Graphs.hpp
@@ -1,17 +1,25 @@
 #ifndef GRAPHS_HPP
 #define GRAPHS_HPP
 
-#include <unordered_map>
-#include <unordered_set>
 #include <string>
-#include <stdexcept>
-#include <algorithm>
 
-class Graphs {
+template <typename TX, typename TY>
+class Graph {
 public:
-  
+    Graph();
+    Graph(TX &x_axis, TY &y_axis);
+    Graph(TX &x_axis, TY &y_axis, std::string &title);
+    
+    std::string GetTitle();
+    TX GetX();
+    TY GetY()
+
+
 private:
-  
+    TX x_axis_;
+    TY y_axis_;
+    std::string title_;
+
 };
 
 #endif
