340049bb320acacd42e1f1782267a9bb4eef7d7e
alemassaad <115092467+alemassaad@users.noreply.github.com>
2022-12-05 14:23:36 +0100

    Create Graphs.hpp

---
+++ src/FrontEnd/Graphs.hpp
@@ -0,0 +1,17 @@
+#ifndef GRAPHS_HPP
+#define GRAPHS_HPP
+
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stdexcept>
+#include <algorithm>
+
+class Token {
+public:
+  
+private:
+  
+};
+
+#endif
