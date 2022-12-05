6b6bf06fc09e9b927389d36ffb99f962137df09a
alemassaad <115092467+alemassaad@users.noreply.github.com>
2022-12-05 15:26:58 +0100

    Update Graphs.cpp

--- src/FrontEnd/Graphs.cpp
+++ src/FrontEnd/Graphs.cpp
@@ -1,2 +1,20 @@
 #include "Graphs.hpp"
 
+template <typename TX, typename TY>
+Graph<TX, TY>::Graph() {
+    x_axis_ = none;
+    y_axis_ = none;
+    title_ = none;
+};
+template <typename TX, typename TY>
+Graph<TX, TY>::Graph(TX &x_axis, TY &y_axis) {
+    x_axis_ = x_axis;
+    y_axis_ = y_axis;
+    title_ = none;
+};
+template <typename TX, typename TY>
+Graph<TX, TY>::Graph(TX &x_axis, TY &y_axis, std::string &title) {
+    x_axis_ = x_axis;
+    y_axis_ = y_axis;
+    title_ = title;
+};
