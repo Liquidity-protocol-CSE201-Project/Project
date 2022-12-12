26ffbb24a6ba1892907aa345dc3e3bacbf14c6c9
victoire-fabien <115148618+victoire-fabien@users.noreply.github.com>
2022-12-12 13:47:16 +0100

    Add files via upload

--- src/FrontEnd/main.cpp
+++ src/FrontEnd/main.cpp
@@ -1,3 +1,15 @@
-int main() {
-  return 0;
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
+
+
+
+
