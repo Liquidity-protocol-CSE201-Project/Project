819d4bd787fa05e21ec4418cc5cb1e673521c484
combi2k2 <cumbee2k2@gmail.com>
2023-01-09 14:43:52 +0100

    add the random price mechanism for token to make it not converge so fast

--- src/Market/Infrastructure/Market.cpp
+++ src/Market/Infrastructure/Market.cpp
@@ -17,15 +17,20 @@ Market::Market() {
     MarketIndex = ++MarketCount;
     epoch = 0;
     A = new CommunityActor(MarketIndex);
+}
+Market::~Market() {
+    delete A;
+    for (auto pool : pools_)    delete pool;
+    for (auto token : tokens_)  delete token;
+    
+    pools_.clear();
+    tokens_.clear();
+}
 
-    // create some initial tokens of the market
-    addToken("USDT", 1);
-    addToken("USDC", 1);
-    addToken("DAI", 1);
-    addToken("ETH", 122);
-    addToken("UNI", 5.19);
-    addToken("MKR", 54.2);
-
+void Market::addInitialScenario(const std::unordered_map<std::string, double> &price_tags, PROTOCOL pool_type) {
+    for (auto [token, price] : price_tags)
+        addToken(token, price);
+    
     for (auto token1 : tokens_)
     for (auto token2 : tokens_) {
         if (token1->name() != token2->name()) {
@@ -38,7 +43,15 @@ Market::Market() {
                 quantities[token1] = 9e7 / token1->real_value();
                 quantities[token2] = 1e8 / token2->real_value(); // each token has equal volumeUSD inside the pool
             }
-            PoolInterface *pool = new UniswapV2Pool(quantities, 0.01);
+            PoolInterface *pool = nullptr;
+
+            if (pool_type == UNISWAP_V2) {
+                pool = new UniswapV2Pool(quantities, 0.01);
+            } else if (pool_type == UNISWAP_V3) {
+                pool = new UniswapV3Pool(quantities, 0.01, 10);
+            } else {
+                throw std::invalid_argument("currently not supporting this protocol");
+            }
 
             addPool(pool);
             A->refill(pool->pool_token(), 1);
@@ -48,15 +61,6 @@ Market::Market() {
     }
     assert(pools_.size() == tokens_.size() * (tokens_.size() - 1) / 2);
 }
-Market::~Market() {
-    delete A;
-    for (auto pool : pools_)    delete pool;
-    for (auto token : tokens_)  delete token;
-    
-    pools_.clear();
-    tokens_.clear();
-}
-
 void Market::addToken(const std::string &name, double price) {
     tokens_.emplace(new Token(name, price));
 }
@@ -70,6 +74,8 @@ void Market::addPool(PoolInterface *pool) {
 }
 
 void Market::runEpoch() {
+    randTokenPrice();
+
     for (auto pool : GetMarketPools()) {
         Token *token1 = *(pool->tokens()).begin();
         Token *token2 = *(++(pool->tokens()).begin());
@@ -78,8 +84,10 @@ void Market::runEpoch() {
         double MarketRatio = token2->real_value() / token1->real_value();
 
         {   // LPs' Behavior:
-            double LP_incentive = abs(log(PoolRatio) - log(MarketRatio));
-            double LP_amount = RV(1, 1) - LP_incentive;
+            const double LEAST_IMBALANCE = 0.02;
+
+            double LP_discourage = abs(log(PoolRatio) - log(MarketRatio));
+            double LP_amount = RV(1, 0.1) + LEAST_IMBALANCE - LP_discourage;
 
             // each pool token worth 1e8 USD so we use constant 0.01 so that the volume of one provision is approximately 1M USD
 
@@ -117,42 +125,48 @@ std::unordered_set<PoolInterface *> Market::GetMarketPools() const {
     return pools_;
 }
 
-// int main() {
-//     Market MySimulation;
+void Market::randTokenPrice() {
+    for (auto token : tokens_)
+        token->real_value_ *= exp(RV(0, 1e-4));
+}
 
-//     std::unordered_map<PoolInterface *, std::vector<double>> poolVal;
-//     std::unordered_map<PoolInterface *, std::vector<double>> liquidity;
+int main() {
+    Market MySimulation;
+    MySimulation.addInitialScenario({{"ETH", 1012}, {"DAI", 10}, {"BTC", 5}}, UNISWAP_V2);
 
-//     for (auto pool : MySimulation.GetMarketPools()) {
-//         Token *token1 = *(pool->tokens()).begin();
-//         Token *token2 = *(++(pool->tokens()).begin());
+    std::unordered_map<PoolInterface *, std::vector<double>> poolVal;
+    std::unordered_map<PoolInterface *, std::vector<double>> liquidity;
 
-//         poolVal[pool] = {2e8};
-//         liquidity[pool] = {pool->GetSpotPrice(token1, token2)};
-//     }
+    for (auto pool : MySimulation.GetMarketPools()) {
+        Token *token1 = *(pool->tokens()).begin();
+        Token *token2 = *(++(pool->tokens()).begin());
 
-//     for (int _ = 0 ; _ < 1000 ; ++_) {
-//         MySimulation.runEpoch();
+        poolVal[pool] = {2e8};
+        liquidity[pool] = {pool->GetSpotPrice(token1, token2)};
+    }
 
-//         for (auto pool : MySimulation.GetMarketPools()) {
-//             Token *token1 = *(pool->tokens()).begin();
-//             Token *token2 = *(++(pool->tokens()).begin());
+    for (int _ = 0 ; _ < 1000 ; ++_) {
+        MySimulation.runEpoch();
+
+        for (auto pool : MySimulation.GetMarketPools()) {
+            Token *token1 = *(pool->tokens()).begin();
+            Token *token2 = *(++(pool->tokens()).begin());
             
-//             poolVal[pool].push_back(token1->real_value() * pool->GetQuantity(token1) + token2->real_value() * pool->GetQuantity(token2));
-//             liquidity[pool].push_back(pool->GetSpotPrice(token1, token2));
-//         }
-//     }
-//     for (auto pool : MySimulation.GetMarketPools()) {
-//         Token *token1 = *(pool->tokens()).begin();
-//         Token *token2 = *(++(pool->tokens()).begin());
-//         std::ofstream file((token1->name() + " and " + token2->name() + ".txt").c_str());
+            poolVal[pool].push_back(token1->real_value() * pool->GetQuantity(token1) + token2->real_value() * pool->GetQuantity(token2));
+            liquidity[pool].push_back(pool->GetSpotPrice(token1, token2));
+        }
+    }
+    for (auto pool : MySimulation.GetMarketPools()) {
+        Token *token1 = *(pool->tokens()).begin();
+        Token *token2 = *(++(pool->tokens()).begin());
+        std::ofstream file((token1->name() + " and " + token2->name() + ".txt").c_str());
 
-//         file << std::fixed << std::setprecision(5);
+        file << std::fixed << std::setprecision(5);
 
-//         for (size_t i = 0 ; i < poolVal[pool].size() ; ++i) {
-//             file << poolVal[pool][i] << " ";
-//             file << liquidity[pool][i] << "\n";
-//         }
-//         file.close();
-//     }
-// }
+        for (size_t i = 0 ; i < poolVal[pool].size() ; ++i) {
+            file << poolVal[pool][i] << " ";
+            file << liquidity[pool][i] << "\n";
+        }
+        file.close();
+    }
+}
--- src/Market/Infrastructure/Market.hpp
+++ src/Market/Infrastructure/Market.hpp
@@ -10,11 +10,12 @@ public:
 
     // the add functions is not used in market simulating process
     // instead is used to create Rug Pull Strategy of Arbitrager
+    void addInitialScenario(const std::unordered_map<std::string, double> &price_tags, PROTOCOL pool_type);
     void addToken(const std::string &name, double price);
     void addToken(Token *token);
     void addPool(PoolInterface *pool);
 
-    virtual void runEpoch();
+    void runEpoch();
 
     std::unordered_set<Token *> GetMarketTokens() const;
     std::unordered_set<PoolInterface *> GetMarketPools() const;
@@ -22,6 +23,8 @@ private:
     static int MarketCount;
     int MarketIndex;
     int epoch;
+
+    void randTokenPrice();
 
     std::unordered_set<Token *> tokens_;
     std::unordered_set<PoolInterface *> pools_;
